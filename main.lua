b105cff856bc05c0fb1891b17c18aabc1e32c496
gdinit <gdinit@gmx.com>
2019-02-09 17:13:40 +0000

    move comments to top. styling improvements

--- main.lua
+++ main.lua
@@ -4,41 +4,42 @@ button = require "dabuton" --Require the library so we can use it.
 
 function love.load()
   local flags = { --Setting up a table called flags with data for our button
+
+    --Position and size
     xPos = 10,
     yPos  = 10,
     width = 100,
     height = 50,
-    --Position and size
 
+    --Color for the button
     color = {
       red = 0,
       green = 0,
       blue = 255,
     },
-    --Color for the button
 
+    --Settings for the border
     border = {
       width = 2,
       red = 0,
       green = 0,
       blue = 0,
     },
-    --Settings for the border
 
     onClick = function()
       print("hai")
     end,
   }
 
-  id = button.spawn(flags)    --Spawn the button
+  id = button.spawn(flags)	-- Spawn the button
   
   id:setPos(200, 200)
 end
 
 function love.update(dt)
-    button.update() --Update all buttons
+    button.update()	-- Update all buttons
 end
 
 function love.draw()
-    button.draw()   --Draw all buttons
+    button.draw()	-- Draw all buttons
 end
